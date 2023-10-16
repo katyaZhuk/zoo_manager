503f77cb201a0e7f8a3f4934537bdf7c1778a871
katyaZhuk <katya310789@gmail.com>
2023-10-16 14:05:26 +0200

    Add application layout

--- ZooManager/MainWindow.xaml.cs
+++ ZooManager/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Windows;
 
 namespace ZooManager
 {
@@ -7,9 +10,15 @@ namespace ZooManager
    /// </summary>
    public partial class MainWindow : Window
    {
+      private readonly LinqToSqlDataClassDataContext _dataContext;
+
       public MainWindow()
       {
          InitializeComponent();
+
+         string connectionString = ConfigurationManager.ConnectionStrings["ZooManager.Properties.Settings.SqlTutorialDBConnectionString"].ConnectionString;
+
+         _dataContext = new LinqToSqlDataClassDataContext(connectionString);
       }
    }
 }
