07328983b6eeb5f465da5a83aac31eca790d7f36
katyaZhuk <katya310789@gmail.com>
2023-10-17 11:28:56 +0200

    Add methods for adding buttons

--- ZooManager/MainWindow.xaml.cs
+++ ZooManager/MainWindow.xaml.cs
@@ -80,5 +80,27 @@ namespace ZooManager
 
          ShowZoos();
       }
+
+      private void AddAnimal_Click(object sender, RoutedEventArgs e)
+      {
+         string newAnimal = addAnimalTextBox.Text;
+         _dataContext.Animals.InsertOnSubmit(new Animal { Name = newAnimal });
+
+         _dataContext.SubmitChanges();
+
+         ShowAnimals();
+      }
+
+      private void AddAnimalToZoo_Click(object sender, RoutedEventArgs e)
+      {
+         Zoo selectedZoo = new Zoo { Location = zoosList.SelectedValue.ToString() };
+         Animal selectedAnimal = new Animal { Name = animalsList.SelectedValue.ToString() };
+
+         _dataContext.AnimalZoos.InsertOnSubmit(new AnimalZoo { Zoo = selectedZoo, Animal = selectedAnimal });
+
+         _dataContext.SubmitChanges();
+
+         ShowAssosiatedAnimals();
+      }
    }
 }
