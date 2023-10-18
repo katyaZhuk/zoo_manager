9a825f8d982651c79e9f89113b115f2104669637
katyaZhuk <katya310789@gmail.com>
2023-10-18 14:14:13 +0200

    Add implemendation for update buttons methods

---
+++ ZooManager/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace ZooManager
+{
+   public static class Constants
+   {
+      public static string DefaultZooTextboxValue = "Enter your Zoo...";
+
+      public static string DefaultAnimalTextboxValue = "Enter your Animal...";
+   }
+}
